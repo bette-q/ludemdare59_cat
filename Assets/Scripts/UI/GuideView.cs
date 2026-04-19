e2012e33da034737a003ed5a5cc349fb13aeb682
TheZodiark <116615603+TheZodiark@users.noreply.github.com>
2026-04-19 20:17:14 +0800

    fix

--- Assets/Scripts/UI/GuideView.cs
+++ Assets/Scripts/UI/GuideView.cs
@@ -10,6 +10,10 @@ public class GuideView : BaseView
     [SerializeField] private Button _foodButton;
     [SerializeField] private Button _toyButton;
 
+    private bool _petNextIsFirstSound = true;
+    private bool _foodNextIsFirstSound = true;
+    private bool _toyNextIsFirstSound = true;
+
     private void Awake()
     {
         _startButton.onClick.AddListener(() =>
@@ -18,8 +22,50 @@ public class GuideView : BaseView
             var view = GameManager.Instance.GetView<MainView>();
             view.ShowBeforeStart();
         });
-        _petButton.onClick.AddListener(() => { AudioManager.Instance.PlayCatRequest(E_CatRequestSound.Petting); });
-        _foodButton.onClick.AddListener(() => { AudioManager.Instance.PlayCatRequest(E_CatRequestSound.Food); });
-        _toyButton.onClick.AddListener(() => { AudioManager.Instance.PlayCatRequest(E_CatRequestSound.Toy); });
+        _petButton.onClick.AddListener(OnPetButtonClicked);
+        _foodButton.onClick.AddListener(OnFoodButtonClicked);
+        _toyButton.onClick.AddListener(OnToyButtonClicked);
+    }
+
+    private void OnPetButtonClicked()
+    {
+        if (_petNextIsFirstSound)
+        {
+            AudioManager.Instance.PlayAttention1();
+        }
+        else
+        {
+            AudioManager.Instance.PlayAttention2();
+        }
+
+        _petNextIsFirstSound = !_petNextIsFirstSound;
+    }
+
+    private void OnFoodButtonClicked()
+    {
+        if (_foodNextIsFirstSound)
+        {
+            AudioManager.Instance.PlayFood1();
+        }
+        else
+        {
+            AudioManager.Instance.PlayFood2();
+        }
+
+        _foodNextIsFirstSound = !_foodNextIsFirstSound;
+    }
+
+    private void OnToyButtonClicked()
+    {
+        if (_toyNextIsFirstSound)
+        {
+            AudioManager.Instance.PlayToy1();
+        }
+        else
+        {
+            AudioManager.Instance.PlayToy2();
+        }
+
+        _toyNextIsFirstSound = !_toyNextIsFirstSound;
     }
 }
